be572e9f00a12a1510a73c59d3f3ed3bbce978d0
Lanxi-Gong <lg3372@columbia.edu>
2025-02-20 21:50:21 -0500

    Update FizzBuzz_File.py

--- FizzBuzz_File.py
+++ FizzBuzz_File.py
@@ -3,9 +3,9 @@ def FizzBuzz(start, finish):
     numbers = np.arange(start, finish + 1)
     result = np.array(numbers, dtype=object)
     
-    result[(numbers % 3 == 0) & (numbers % 5 != 0)] = 'Fizz'
-    result[(numbers % 5 == 0) & (numbers % 3 != 0)] = 'Buzz'
-    result[(numbers % 3 == 0) & (numbers % 5 == 0)] = 'FizzBuzz'
+    result[(numbers % 3 == 0) & (numbers % 5 != 0)] = 'fizz'
+    result[(numbers % 5 == 0) & (numbers % 3 != 0)] = 'buzz'
+    result[(numbers % 3 == 0) & (numbers % 5 == 0)] = 'fizzbuzz'
     
     return result.tolist()
 
