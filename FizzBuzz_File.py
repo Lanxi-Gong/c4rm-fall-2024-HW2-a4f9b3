4080975b71235d3a83293c93837d7e512d8f4def
Lanxi-Gong <lg3372@columbia.edu>
2025-02-20 21:37:45 -0500

    Update FizzBuzz_File.py

--- FizzBuzz_File.py
+++ FizzBuzz_File.py
@@ -1,11 +1,10 @@
 import numpy as np
 def FizzBuzz(start, finish):
-    numvec = np.arange(start, finish+1)
-    objvec = np.array(numvec, dtype=object)
-
-    objvec[(numvec % 3 == 0) & (numvec % 5 != 0)] = 'Fizz'
-    objvec[(numvec % 5 == 0) & (numvec % 3 != 0)] = 'Buzz'
-    objvec[(numvec % 3 == 0) & (numvec % 5 == 0)] = 'FizzBuzz'
-
-    return objvec
-
+    numbers = np.arange(start, finish + 1)
+    result = np.array(numbers, dtype=object)
+    
+    result[(numbers % 3 == 0) & (numbers % 5 == 0)] = "fizzbuzz"
+    result[numbers % 3 == 0] = "fizz"
+    result[numbers % 5 == 0] = "buzz"
+    
+    return result.tolist()
